3e4603bc0bc264c09b44b5b829fd2662e5db17cf
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-08-22 10:06:03 -0300

    melhorando o formulário de login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,18 +3,19 @@ import * as React from 'react'
 import signin__img from '../../assets/images/img-signin.png'
 import Container from '../../components/Container'
 import { Link, useNavigate } from 'react-router-dom'
-import { useProfile } from '../../hooks/useProfile'
 
 export default function SignIn() {
-
-    const { signIn, setUser } = useProfile()
-
     const navigate = useNavigate()
 
     const [form, setForm] = React.useState({
         email: '',
         password: ''
     })
+
+    const [warning, setWarning] = React.useState({
+        show: false,
+        message: ''
+    });
 
     const handleChange = (e) => {
         setForm({
@@ -25,13 +26,42 @@ export default function SignIn() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (signIn(form)) {
-            localStorage.setItem('token', true)
-            navigate('/home')
+
+        let users = []
+
+        if (localStorage.getItem('users')) {
+            users = JSON.parse(localStorage.getItem('users'))
+
+            const user = users.find(u => u.email === form.email && u.password === form.password)
+
+            if (user) {
+                localStorage.setItem('token', user.email)
+                setWarning({
+                    show: false,
+                    message: ''
+                })
+                navigate('/profile')
+            } else {
+                setWarning({
+                    show: true,
+                    message: 'Usuário ou senha inválidos!'
+                })
+            }
         } else {
-            alert('E-mail ou senha inválidos!')
+            setWarning({
+                show: true,
+                message: 'Nenhum usuário cadastrado!'
+            })
         }
     }
+
+    /*
+        Ao clicar para submeter o formulário, entra no handleSubmit, usa o preventDefault, inicializa o array users vazio e verifica se existem usuários cadastrados
+        Se existir, pega esses usuários, salva dentro do array users e faz a verificação se dentro desse array de usuários existe o usuário que deseja logar
+        Se encontrar, a variável user será verdadeira, entrando dentro do if, se não encontrar, será falso, entra no else e exibe a mensagem 'Usuário ou senha inválidos!'
+        Caso não possua nenhum usuário cadastrado, quando digitar para entrar, localStorage.getItem('users') será falso, pois o users ainda não existe
+        Como não existe, cai no else, a parte do warning vai recarregar, o warning.show será verdadeiro e exibirá a mensagem 'Nenhum usuário cadastrado!'
+    */
 
     return (
         <Container>
@@ -54,6 +84,7 @@ export default function SignIn() {
                         value={form.email}
                         onChange={handleChange}
                     />
+
                     <label htmlFor="password" className="sign-in__label">
                         Senha
                     </label>
