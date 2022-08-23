039540e0a460cebb8e02d79ffe70ce2e55ae27ca
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-08-23 10:57:34 -0300

    adicionando as rotas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import MyRoutes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
+    <MyRoutes />
   </React.StrictMode>
 );
 
