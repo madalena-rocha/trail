86b06d825f316321363364a7a75a54c960f33419
Jean Wilker <jeanwilkerss@gmail.com>
2022-08-23 21:54:41 -0300

    Adiciona nomes dos locais nas imagens do banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,9 +1,9 @@
 import './style.css'
-import sidney from '../../assets/images/Sydney.svg';
-import bolivia from '../../assets/images/Bolivia.svg';
-import peru from '../../assets/images/Peru.svg';
-import berlim from '../../assets/images/Berlim.svg';
-import rio from '../../assets/images/Rio.svg';
+import sidney from '../../assets/images/sydney.svg';
+import bolivia from '../../assets/images/bolivia.svg';
+import peru from '../../assets/images/peru.svg';
+import berlim from '../../assets/images/berlim.svg';
+import rio from '../../assets/images/rio.svg';
 import Gradient from '../../components/Gradient';
 
 const Banner = () => {
@@ -12,30 +12,31 @@ const Banner = () => {
       
       <Gradient/>
 
-      <div className='banner__box'>
-        <img src={sidney} alt="" />
-        <span></span>
+      <div className='banner__card'>
+        <img className='banner__card___img' src={sidney} alt="" />
+        <h2 className='card__country'>Sidney</h2>
       </div>
 
-      <div className='banner__box'>
-        <img src={bolivia} alt="" />
-        <span></span>
+      <div className='banner__card'>
+        <img className='banner__card___img' src={bolivia} alt="" />
+        <h2 className='card__country'>Bolivia</h2>
       </div>
 
-      <div className='banner__box'>
-        <img src={peru} alt="" />
-        <span></span>
+      <div className='banner__card'>
+        <img className='banner__card___img' src={peru} alt="" />
+        <h2 className='card__country'>Peru</h2>
       </div>
 
-      <div className='banner__box'>
-        <img src={berlim} alt="" />
-        <span></span>
+      <div className='banner__card'>
+        <img className='banner__card___img' src={berlim} alt="" />
+        <h2 className='card__country'>Berlim</h2>
       </div>
 
-      <div className='banner__box'>
-        <img src={rio} alt="" />
-        <span></span>
-      </div>
+      <div className='banner__card'>
+        <img className='banner__card___img' src={rio} alt="Rio" />
+        <h2 className='card__country'>Rio</h2>
+  </div>
+
     </section>
   );
 }
