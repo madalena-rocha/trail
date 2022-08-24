5bb85d493d524e1b09ae254fc95f3802e0f626c9
Jean Wilker <jeanwilkerss@gmail.com>
2022-08-23 21:16:51 -0300

    Modifica className e estrutura html dos cards de publicação

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,7 +1,8 @@
 import './style.css'
-import icon from '../../assets/images/Icon-filter.svg'
-import like from '../../assets/images/Heart.svg'
-import comment from '../../assets/images/Comment.svg'
+import {BiDownArrow} from 'react-icons/bi'
+import {AiOutlineEye} from 'react-icons/ai'
+import {BiCommentDetail} from 'react-icons/bi'
+
 
 const Posts = () => {
   return (
@@ -9,39 +10,44 @@ const Posts = () => {
 
       <div className='posts__filter'>
         <button className='filter__dropdown'>Filtro
-          <img className='filter__icon' src={icon} alt=''></img>
+          <BiDownArrow/>
         </button>
       </div>
 
       <div className='posts__row'>
 
-        <div className='row__box'>
-          <div className='box__perfil'>
-            <div className='perfil__photo'></div>
-            <div className='perfil__info'>
+        <div className='row__card'>
+          <div className='card__perfil'>
+            <div className='card__perfil___photo'></div>
+            <div className='card__perfil___info'>
               <span className='info__name'>@devjunior</span>
               <br />
               <span className='info__date'>12/08/2022</span>
             </div>
           </div>
-
-          <div className='box__text'>
-            <span className='text__localization'>Bahia, Brasil</span>
-            <p className='text__contents'>Lorem ipsum dolor sit amet, consectetur 
+          
+          <div className='card__intro'>
+            <h4 className='card__intro___location'>Bahia, Brasil</h4>
+            <p className='card__intro___text'>Lorem ipsum dolor sit amet, consectetur 
             adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua...</p>
-
-            <div className='text__icons'>
-              <img className='icons__comments' src={comment} alt="Ícone de comentários." />
-              <img className='icons__like' src={like} alt="Ícone de like." />
+            
+            <div className='intro__view'>
+              <button href='#' className='intro__view___btn'>Ver mais</button>
+              
+              <div className='intro__icons'>
+                <AiOutlineEye/>
+                <BiCommentDetail/>
+              </div>
             </div>
-          </div>  
+          
+          </div>       
         </div>
 
       </div>
 
       <div className='posts__load'>
-        <button className='load__btn'>Carregar mais...</button>
+        <button className='posts__load___btn'>Carregar mais...</button>
       </div>
   
     </section>
