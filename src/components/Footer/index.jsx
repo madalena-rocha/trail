cc8bc8e956194e844d0ccde70846e004227baf8a
Jean Wilker <jeanwilkerss@gmail.com>
2022-08-24 03:45:20 -0300

    cria copyright do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import './style.css'
+import logo from '../../assets/images/logo-thrail.svg'
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <img className="footer__logo" src="" alt="" />
+    <footer className="footer"> 
+        <p className="footer__copyright">&copy; 2022 Thrail. All Rights Reserved.</p>
     </footer>
   )
 }
