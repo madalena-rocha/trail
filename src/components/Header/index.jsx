e6b6fb6e8d4787d5640156093910750bf8a620fe
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-08-25 10:48:55 -0300

    linkando o formulário de login  ao botão de login/cadastro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,19 @@
 import './style.css';
 import {BiSearch} from 'react-icons/bi'
 import logo from '../../assets/images/logo-trail.svg'
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/sign-in');
+}
+
   return (
     <header className="header">
       
-        <img className='header__logo___img' src={logo} alt="Logo do site" />
+      <img className='header__logo___img' src={logo} alt="Logo do site" />
 
       <form action="" className="header__search">
         <input type="text" name="search" id="search" placeholder="Pesquisar"/>
@@ -14,7 +21,8 @@ const Header = () => {
           <BiSearch/>
         </button> 
       </form>
-      <button className="header__access___btn">Cadastro / Login</button>
+      
+      <button className="header__access___btn" onClick={() => handleClick()}>Cadastro / Login</button>
     </header>
   );
 }
