876ef94f7dd911223e29546ee4d1220d7e0ac20b
Ana <annamarya461@gmail.com>
2022-08-25 15:40:48 -0300

    atualizando página de perfil

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -2,14 +2,32 @@
 import './styles.css' 
 import Profile from '../../components/Profile'
 import BackgroundProfile from '../../components/BackgroundProfile'
+import Header from '../../components/Header'
+import Posts from '../../components/Posts'
+import Division from '../../components/Division'
+import Footer from '../../components/Footer'
+import Main from '../../components/Main'
+import Container from '../../components/Container'
+import { useState } from 'react'
 
 
 export default function Perfil () {
+
+    const [following, setFollowing] = useState(false);
+
     return (
 
-        <div className="container">
-            <BackgroundProfile/>
-            <Profile/>
+        <div className="Perfil">
+            <Container>
+                <Header/>
+                    <Main>   
+                        <BackgroundProfile/> 
+                        <Profile following={following} onClick={setFollowing}/>
+                        <Division title='Publicações' /> 
+                        <Posts/>
+                    </Main>
+                <Footer/>
+            </Container>   
         </div>
     ) 
 }
