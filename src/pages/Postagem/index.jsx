d0c16b24cf3f746e378be2a91cd0ced100148b24
Sócrates Costa <socratesaoc@gmail.com>
2022-08-31 20:00:06 -0300

    Inicio Postagem

---
+++ src/pages/Postagem/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+
+
+export default function Postagem() {
+  return (
+    <div className="container-postagem">      
+   
+    </div> 
+    );
+}
