13b2220df16280f2cb0f6742f5b8abb16fd1c194
Jean Wilker <jeanwilkerss@gmail.com>
2022-08-31 22:19:19 -0300

    Finalizando modal do banner

--- src/components/ModalBanner/index.jsx
+++ src/components/ModalBanner/index.jsx
@@ -13,6 +13,7 @@ const ModalBanner = (props) => {
         </div>
         <div className='modal__banner___contents'>
           <h4 className='modal__banner___title'>{props.content.location}</h4>
+          <div className='modal__banner___line'></div>
           <p className='modal__banner___text'>{props.content.text}</p>
         </div>
         <button className='modal__banner___btn' 
